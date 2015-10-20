a2a810acb5af223084ec6256749ce40e53ab87f9
Pow Chow <pauline.chow@gmail.com>
2015-10-20 08:18:50 +0000

    added run

---
+++ run2.sh
@@ -0,0 +1,47 @@
+CORES=2
+OUTPUT=${OUTPUT:-results}
+
+array=(
+    ContinuousPeaksTest
+#    CountOnesTest
+#    FlipFlopTest
+#    FourPeaksTest
+#    KnapsackTest
+#    MaxKColoringTest 
+#    NQueensTest
+#    TravelingSalesmanTest
+)
+
+mkdir -p ${OUTPUT}
+mkdir -p ${OUTPUT}
+
+count=0
+
+function set_header {
+    if [[ $1 = "AssignmentTwo" ]]; then
+        echo "algorithm,iterations,training error,testing error,training time,testing time" > ${OUTPUT}/${1}.csv
+    else
+        echo "algorithm,iterations,time,optim" > ${OUTPUT}/${1}.csv
+    fi
+}
+
+for j in "${array[@]}";do
+    for i in $(seq 1);do
+        count=$((count+1))
+
+        if [[ $count -eq 0 ]]; then
+            set_header ${j}
+        fi
+
+        java -cp ABAGAIL.jar opt.test.${j} ${i} >| /tmp/${j}${i}.csv &
+
+        if [[ $(expr $count % ${CORES}) -eq 0 ]]; then
+            # wait for cores to free up
+            wait
+        fi
+    done
+    # catch overflow
+    wait
+    cat /tmp/${j}*.csv >> /tmp/${OUTPUT}/${j}.csv
+    rm -f /tmp/${j}*.csv
+done
